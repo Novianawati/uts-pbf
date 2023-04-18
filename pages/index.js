52e9620323973b26bb9f15b9bcc1854cd494a382
Novianawati <novianawati92@gmailcom>
2023-04-19 02:01:43 +0700

    first

--- pages/index.js
+++ pages/index.js
@@ -5,46 +5,40 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>UTS PBF</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="">Indonesian Library</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="" className={styles.card}>
+            <h3>Koleksi &rarr;</h3>
+            <p>Pendataan koleksi buku</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="" className={styles.card}>
+            <h3>Riwayat peminjaman &rarr;</h3>
+            <p>Riwayat peminjaman buku</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href=""
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>Keanggotaan &rarr;</h3>
+            <p>Pengelolaan data keanggotaan perpustakaan</p>
+          </a>
+
+          <a
+            href=""
+            className={styles.card}
+          >
+            <h3>Tamu &rarr;</h3>
+            <p>Pengelolaan data tamu perpustakaan</p>
           </a>
         </div>
       </main>
@@ -55,8 +49,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+          UTS Pemograman Berbasis Framework : Nesa itfirul lail & Novianawati
         </a>
       </footer>
 
